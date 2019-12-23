57be6a4cde2128ee07f3126fa055eba5e0dfd82d
sendak <vivianmahr@gmail.com>
2019-12-23 00:40:49 -0800

    new tiled format

--- js/levels/generate_maps.py
+++ js/levels/generate_maps.py
@@ -1,45 +1,73 @@
 import os
+import xml.etree.ElementTree as ET
 
-LEVELS_DIR = ".\\json\\"
-
-def is_json(filename):
-    return filename[-5:] == ".json"
+CURRENT = os.getcwd()
+LEVELS_DIR = CURRENT + "/maps"
+ASSETS_DIR = CURRENT + "/tilesets/"
 
 def readfile(filename):
+    # gets str in a file
     file = open(filename)
     results = file.read()
     file.close()
     return results
 
 def gen_results(path):
+    # Gets all the files in the specified path
     results = []
     for obj in os.listdir(path):
         new_path = os.path.join(path, obj)
         if os.path.isdir(new_path):
             results += gen_results(new_path)
-        elif is_json(new_path):
+        else:
             results.append(new_path)
     return results
 
-def get_name(path):
-    path = path.split('\\')
-    return path[-1][:-5]
+def to_xml_info(filename):
+    # formats relevant information into a json string
+    root = ET.parse(filename).getroot()
+    name = filename.split("/")[-1][:-4]
+    tileset = root.find("tileset").attrib["source"].split("/")[-1]
+    result = '\t"' + name + '": {\n'
+    result += '\t\t"name": "' + name + '",\n'
+    result += '\t\t"tileset": "' + tileset + '",\n' 
+    result += '\t\t"layers": {\n'
+    for layer in root.findall('layer'):
+        result += '\t\t\t"' + layer.attrib["name"] + '": [\n'
+        result += '\t\t\t\t"' + layer.find("data").text.replace("\n", "") + "\n"
+        result += '\t\t\t],\n'
+    result += '\t\t}\n'
+    result += '\t}'
+    return result
 
 def generate_map_file(levels):
-    result = "define([],\nfunction()\n{"
-    filenames = []
+    result = "define([],\nfunction()\n{\n"
     for level in levels:
-        name = get_name(level)
-        filenames.append(name)
-        result += "\n" + name + " = " + readfile(level)
-    result += "\n\nreturn {\n"
-    for file in filenames:
-        result += "\t{0} : {0},\n".format(file)
-    result += "\n    };\n});"
+        result += to_xml_info(level) + ",\n"
+    result += "});"
     file = open("maps.js", "w")
     file.write(result)
     file.close()
     
+def rewrite_assets(tilesets):
+    for tileset in tilesets:
+        pass
+        # tilset.js file, similar to levels. load tileset on request
+        # offsets for all the tile ids
+        # json list as well
+        """
+        tilesets: {
+            tileset_name: 
+                {
+                    x_offset:
+                    y_offset: 
+                    properties: 
+                }
+            ]
+        }
+        """
 
-d = gen_results(LEVELS_DIR)
-generate_map_file(d)
+maps = gen_results(LEVELS_DIR)
+generate_map_file(maps)
+tilesets = gen_results(ASSETS_DIR)
+rewrite_assets(tilesets)
---
+++ js/levels/tilesets/BaseTiles.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="BaseTiles" tilewidth="24" tileheight="24" tilecount="64" columns="8">
+ <image source="../../../images/BaseTiles.png" width="192" height="192"/>
+ <tile id="1">
+  <properties>
+   <property name="spawn" value="testEntity2"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="spawn" value="testEntity"/>
+  </properties>
+ </tile>
+</tileset>
